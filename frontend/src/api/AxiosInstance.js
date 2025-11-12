7a4005f89be700f47329cf6eb1750e5c8bd4e6cc
alwaysandy <andrewdaurio@gmail.com>
2025-11-12 12:03:41 -0500

    Implement Employee axios API

---
+++ frontend/src/api/AxiosInstance.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const BACKEND_BASE_API = "http://localhost:3001/api/v1";
+
+const axiosInstance = axios.create({
+    baseURL: BACKEND_BASE_API,
+    timeout: 2000,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+        // You can modify the request config here if needed
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+axiosInstance.interceptors.response.use(
+    (response) => {
+        // You can process the response data here if needed
+        return response;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default axiosInstance;
---
+++ frontend/src/api/employees/EmployeeAPI.js
@@ -0,0 +1,51 @@
+import axiosInstance from '../AxiosInstance';
+
+const EmployeeAPI = {
+    fetchEmployees: async() => {
+        try {
+            const response = await axiosInstance.get('/employees');
+            if (response.data.status) return response.data.data;
+            else throw new Error(response.data.message);
+        } catch (error) {
+            throw new Error('Error fetching employees: ' + error.message);
+        }
+    },
+    fetchEmployeeById: async(employeeId) => {
+        try {
+            const response = await axiosInstance.get(`/employees/${employeeId}`);
+            if (response.data.status) return response.data.data;
+            else throw new Error(response.data.message);
+        } catch (error) {
+            throw new Error('Error fetching employee: ' + error.message);
+        }
+    },
+    addEmployee: async(employee) => {
+        try {
+            const response = await axiosInstance.post('/employees', employee);
+            if (response.data.status) return response.data.data;
+            else throw new Error(response.data.message);
+        } catch (error) {
+            throw new Error('Error adding employee: ' + error.message);
+        }
+    },
+    updateEmployee: async(employeeId, employeeData) => {
+        try {
+            const response = await axiosInstance.put(`/employees/${employeeId}`, employeeData);
+            if (response.data.status) return response.data.data;
+            else throw new Error(response.data.message);
+        } catch (error) {
+            throw new Error('Error updating employee: ' + error.message);
+        }
+    },
+    deleteEmployee: async(employeeId) => {
+        try {
+            const response = await axiosInstance.delete(`/employees/${employeeId}`);
+            if (response.data.status) return response.data.data;
+            else throw new Error(response.data.message);
+        } catch (error) {
+            throw new Error('Error deleting employee: ' + error.message);
+        }
+    }
+}
+
+export default EmployeeAPI;
